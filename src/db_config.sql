e264b91beb93edf37a294d2e424719fb83c4c6e5
Antonio C <acheong@student.dalat.org>
2021-10-18 17:14:45 +0800

    Add client accounts table and reported users/messages table. Also done with admin login backend

--- src/db_config.sql
+++ src/db_config.sql
@@ -1,26 +1,42 @@
 DROP TABLE IF EXISTS admin_accounts;
 CREATE TABLE admin_accounts (
-  USER_ID INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT NOT NULL,
-  USERNAME varchar(20) UNIQUE NOT NULL,
-  PASSWORD_HASH varchar(32) NOT NULL,
-  SETTINGS INTEGER
+  user_id INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT NOT NULL,
+  username varchar(20) UNIQUE NOT NULL,
+  password_hash varchar(32) NOT NULL,
+  settings INTEGER
+);
+DROP TABLE IF EXISTS client_accounts;
+CREATE TABLE admin_accounts (
+  user_id INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT NOT NULL,
+  username varchar(20) UNIQUE NOT NULL,
+  password_hash varchar(32) NOT NULL,
+  settings INTEGER
 );
 DROP TABLE IF EXISTS article;
 CREATE TABLE article (
-  ID INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT NOT NULL,
-  TITLE varchar(70) UNIQUE NOT NULL,
-  DESCRIPTION TEXT NOT NULL,
-  CONTENT BLOB NOT NULL,
-  AUTHOR varchar(30) NOT NULL,
-  CREATION_DATE real NOT NULL,
-  SETTINGS integer
+  id INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT NOT NULL,
+  title varchar(70) UNIQUE NOT NULL,
+  description TEXT NOT NULL,
+  content BLOB NOT NULL,
+  author varchar(20) NOT NULL,
+  creation_date real NOT NULL,
+  settings integer
 );
 DROP TABLE IF EXISTS article_comments;
 CREATE TABLE article_comments (
-  ID INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT NOT NULL,
-  TITLE varchar(70) UNIQUE NOT NULL,
-  CONTENT TEXT NOT NULL,
-  AUTHOR varchar(30) NOT NULL,
-  CREATION_DATE real NOT NULL,
-  SETTINGS integer
+  id INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT NOT NULL,
+  title varchar(70) UNIQUE NOT NULL,
+  content TEXT NOT NULL,
+  author varchar(20) NOT NULL,
+  creation_date real NOT NULL,
+  settings integer
 );
+DROP TABLE IF EXISTS reports;
+CREATE TABLE reports (
+  id INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT NOT NULL,
+  title varchar(70) UNIQUE NOT NULL,
+  details TEXT NOT NULL,
+  author_id INTEGER NOT NULL,
+  reported_user_id INTEGER NOT NULL,
+  reported_message_id INTEGER NOT NULL
+)
--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, url_for, flash, redirect, mak
 from flask.ext.session import Session
 from werkzeug.exceptions import abort
 import os.path
-import hashlib
+from hashlib import md5
 from werkzeug.utils import secure_filename
 
 #Configs
@@ -15,17 +15,58 @@ app.secret_key = "0de03e1a949f142951868617004aa54b"
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 #Utility functions
-
+def getMD5(plaintext):
+    m = md5()
+    m.update(plaintext.encode('utf-8'))
+    hash = str(m.hexdigest())
+    return hash
 
 #App functions
-def unauthenticated(route):
-    return redirect(url_for('login'))
-
+def authenticate_admin(username, hashpass):
+    #Connect to sql database and get username and password
+    conn = sqlite3.connect('database.db')
+    cur = conn.cursor()
+    dbhash = str(cur.execute('SELECT password_hash FROM admin_accounts WHERE username = ?', (username,)).fetchone())
+    if hashpass == dbhash:
+        return True
+    else:
+        return False
 #App routes
+    #Admin login
+@app.route('/admin/login')
+def admin_login():
+    if 'admin_id' not in session:
+        if request.method == 'GET':
+            return render_template('admin/login.html')
+        elif request.method == 'POST':
+            username = request.form['username']
+            password = request.form['password']
+            hashpass = getMD5(password)
+            correct = authenticate_admin(username, hashpass)
+            if correct == True:
+                session['admin_id'] = username
+                flash('Logged in')
+                return redirect(url_for('admin_index'))
+            elif correct == False:
+                flash('Incorrect username or password')
+                return redirect(url_for('admin_login'))
+            else:
+                flash('Error')
+                return redirect(url_for('admin_login'))
+
+    #Admin index
 @app.route('/admin', methods=('GET', 'POST'))
-def admin():
-    route = "/admin"
-    if 'username' not in session:
-        unauthenticated(route)
-    elif 'username' in session:
-        return render_template('admin.html')
+def admin_index():
+    if 'admin_id' not in session:
+        return redirect(url_for('admin_login'))
+    elif 'admin_id' in session:
+        if request.method == 'GET':
+            return render_template('admin/index.html')
+        #Administrative actions
+        elif request.method == 'POST':
+            action = request.form['action']
+            #Deleting comments, banning users from commenting, etc
+        return redirect(url_for('admin_index'))
+    else:
+        flash('Error')
+        redirect(url_for('admin_index'))
