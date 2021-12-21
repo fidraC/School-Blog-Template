3ddb50a1275d9ca77747043ae154f7e184163c2e
Antonio C <acheong@student.dalat.org>
2021-12-21 11:33:46 +0800

    Change default password alert

--- src/reset_db.py
+++ src/reset_db.py
@@ -6,7 +6,7 @@ connection = sqlite3.connect('database.db')
 with open('db_config.sql') as f:
     connection.executescript(f.read())
 cur = connection.cursor()
-#Root password = ROOTadmin16!
+#Root password = ROOTadmin16! (Change for production)
 cur.execute('INSERT INTO admin_accounts (username, password_hash, department) VALUES (?, ?, ?)',
     ('root', '7f2c06514be0f80aefcb494e1a6c9c2c', 'root'))
 
